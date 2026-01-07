278188bb8f82bbcbddd4cb33d25ceedd08863ecd
Tomer Horowitz <tomerh@realcommerce.co.il>
2026-01-07 11:34:10 +0200

    fixx: add bun as a dependency and update related files
    - Added "bun": "^1.3.5" to package.json dependencies.
    - Updated yarn.lock to include bun and its platform-specific binaries.
    - Modified test to change the documentation URL from wix to tomerh2001 for consistency.

--- tests/lib/util/getDocsUrl.js
+++ tests/lib/util/getDocsUrl.js
@@ -6,7 +6,7 @@ const getDocsUrl = require('../../../src/util/getDocsUrl')
 
 describe('getDocsUrl', () => {
     it('returns the URL of the a named rule\'s documentation', () => {
-        const url = `https://github.com/wix/eslint-plugin-lodash/blob/v${pkg.version}/docs/rules/foo.md`
+        const url = `https://github.com/tomerh2001/eslint-plugin-lodash/blob/v${pkg.version}/docs/rules/foo.md`
         assert.strictEqual(getDocsUrl('foo'), url)
     })
 })
